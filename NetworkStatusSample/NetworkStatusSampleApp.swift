dfe885f01a0c9c104b89980509adfb607f71abd6
bing <bindaguo@gmail.com>
2023-01-15 15:39:47 +0800

    Initial Commit

---
+++ NetworkStatusSample/NetworkStatusSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkStatusSampleApp.swift
+//  NetworkStatusSample
+//
+//  Created by Bing Guo on 2023/1/15.
+//
+
+import SwiftUI
+
+@main
+struct NetworkStatusSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
